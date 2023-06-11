66573ba27195cab70f9abaa5c0c90816d8768fe1
Liliia Korotina <liliya.korotina96@gmail.com>
2023-06-11 22:01:23 +0200

    uodate

--- lib/src/theme/app_colors.dart
+++ lib/src/theme/app_colors.dart
@@ -4,17 +4,10 @@ class AppColors {
   const AppColors._();
 
   static const Color white = Color(0xFFFFFFFF);
-  static const Color riverBed = Color(0xFF44515E);
-
-  static const Color bluishPurple = Color(0xFF1C1724);
-  static const Color purplyMagenta = Color(0xFF2A1E2B);
-  static const Color darkPurple = Color(0xFF300E44);
-  static const Color rouge = Color(0xFF963D74);
-
-  static const Color macawBlueGreen = Color(0xFF42C0B2);
-  static const Color gableGreen = Color(0xFF1B3335);
-  static const Color dark = Color(0xFF1B2629);
-
   static const Color fern = Color(0xFF5FB471);
   static const Color fadedJade = Color(0xFF3B7186);
+  static const Color pink = Color(0xFFe997b0);
+  static const Color orange = Color(0xFFFcc6a2);
+  static const Color whitish = Color(0xFFfbf9fa);
+
 }
