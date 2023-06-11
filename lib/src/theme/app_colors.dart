4f9af5ae7777092d9ac94fad501e6c1ecb4ad886
Liliia Korotina <liliya.korotina96@gmail.com>
2023-06-11 22:54:50 +0200

    uodate

--- lib/src/theme/app_colors.dart
+++ lib/src/theme/app_colors.dart
@@ -7,7 +7,7 @@ class AppColors {
   static const Color fern = Color(0xFF5FB471);
   static const Color fadedJade = Color(0xFF3B7186);
   static const Color pink = Color(0xFFe997b0);
-  static const Color orange = Color(0xFFFcc6a2);
+  static const Color orange = Color(0xfffdd8bf);
   static const Color whitish = Color(0xFFfbf9fa);
 
 }
