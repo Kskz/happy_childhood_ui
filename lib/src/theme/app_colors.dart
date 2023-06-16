ba43237a827394cdbf367c5dc1f5938a17ae34d3
Korotina Liliia <lkorotina@visionable.com>
2023-06-16 01:08:57 +0100

    colors

--- lib/src/theme/app_colors.dart
+++ lib/src/theme/app_colors.dart
@@ -4,12 +4,15 @@ class AppColors {
   const AppColors._();
 
   static const Color white = Color(0xFFFFFFFF);
+  static const Color green = Color(0xFFb2ebc2);
   static const Color fern = Color(0xFF5FB471);
   static const Color fadedJade = Color(0xFF3B7186);
   static const Color pink = Color(0xffe3a3b7);
   static const Color orange = Color(0xfffdd8bf);
   static const Color whitish = Color(0xFFfbf9fa);
+  static const Color litePurple = Color(0xFFdbd5ef);
+  static const Color bluish = Color(0xFFd1e0fd);
+  static const Color purple= Color.fromRGBO(160, 168, 215, 1);
   static const Color chetWodBlue= Color.fromRGBO(95, 107, 186, 1);
   static const Color endeavour = Color.fromRGBO(44, 86, 160, 1);
-
 }
