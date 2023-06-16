7efeedbb25aa5ad912f0bbb6473da26e6dec742d
Korotina Liliia <lkorotina@visionable.com>
2023-06-16 10:22:23 +0100

    dropdown

---
+++ lib/src/widgets/controls/dropdown.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+
+class DropDown<T> extends StatefulWidget {
+  DropDown({
+    Key? key,
+    required this.hint,
+    required this.items,
+    required this.onChanged,
+    required this.selectedValue,
+    required this.textStyle,
+    this.isExpended = false,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  final String hint;
+  final List<DropdownMenuItem<T>> items;
+  final void Function(T) onChanged;
+  final T selectedValue;
+  final TextStyle textStyle;
+  final bool isExpended;
+  final double? width;
+  final double? height;
+
+  @override
+  State<DropDown<T>> createState() => _DropDownState<T>();
+}
+
+class _DropDownState<T> extends State<DropDown<T>> {
+  late T selectedValue;
+
+  @override
+  void initState() {
+    selectedValue = widget.selectedValue;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: widget.height,
+      width: widget.width,
+      child: DropdownButton2<T>(
+        isExpanded: widget.isExpended,
+        hint: Text('Select Item'),
+        style: widget.textStyle,
+        value: selectedValue,
+        onChanged: (value) {
+          if (value != null) {
+            setState(() {
+              selectedValue = value;
+              widget.onChanged(value);
+            });
+          }
+        },
+        items: widget.items,
+      ),
+    );
+  }
+}
