5d10a106f5b49c2c2ba7ec802c2986f8cfb3e9bc
Korotina Liliia <lkorotina@visionable.com>
2023-06-16 01:40:03 +0100

    colors

--- lib/src/theme/app_colors.dart
+++ lib/src/theme/app_colors.dart
@@ -3,13 +3,14 @@ import 'package:flutter/widgets.dart';
 class AppColors {
   const AppColors._();
 
+  static const Color gray = Color(0xFFF0F1F3);
+  static const Color whitish = Color(0xFFfbf9fa);
   static const Color white = Color(0xFFFFFFFF);
   static const Color green = Color(0xFFb2ebc2);
   static const Color fern = Color(0xFF5FB471);
   static const Color fadedJade = Color(0xFF3B7186);
   static const Color pink = Color(0xffe3a3b7);
   static const Color orange = Color(0xfffdd8bf);
-  static const Color whitish = Color(0xFFfbf9fa);
   static const Color litePurple = Color(0xFFdbd5ef);
   static const Color bluish = Color(0xFFd1e0fd);
   static const Color purple= Color.fromRGBO(160, 168, 215, 1);
