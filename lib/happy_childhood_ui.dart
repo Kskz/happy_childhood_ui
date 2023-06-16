06336affe2bd77f1cf94901dd595202252207ed3
Korotina Liliia <lkorotina@visionable.com>
2023-06-16 10:22:46 +0100

    dropdown

--- lib/happy_childhood_ui.dart
+++ lib/happy_childhood_ui.dart
@@ -14,6 +14,7 @@ export 'src/widgets/login/login_submit_button.dart';
 export 'src/widgets/buttons/round_button.dart';
 export 'src/widgets/controls/switcher.dart';
 export 'src/widgets/controls/toggle.dart';
+export 'src/widgets/controls/dropdown.dart';
 export 'src/widgets/sliver_refresh_indicator_widget.dart';
 export 'src/widgets/buttons/hc_action_button.dart';
 export 'src/widgets/input/hc_text_input.dart';
